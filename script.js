5a38d61bdd178af3c039172ac48cd6ecf9ef2021
vale690 <valeryscausilberrio@gmail.com>
2025-11-09 19:18:39 -0500

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Interactividad mínima para la boutique
+// Boutique - interactividad
 document.getElementById('year').textContent = new Date().getFullYear();
 const menuBtn = document.getElementById('menuBtn');
 const menu = document.getElementById('menu');
 menuBtn.addEventListener('click',()=> menu.classList.toggle('hidden'));
-
 function addFav(item){
-  alert(item + ' añadido a favoritos ✨');
+  alert(item + ' añadido a tus favoritos ✨');
 }
-
+function ver(item){
+  alert('Mostrando detalles de ' + item + '. (Demo)');
+}
 document.getElementById('contactForm').addEventListener('submit', e=>{
   e.preventDefault();
-  alert('Gracias por escribirnos — te responderemos pronto.');
+  alert('Gracias por escribir — te responderemos en breve.');
   e.target.reset();
 });
